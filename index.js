b6408f87bd08d1b3ea5d2e684ee89ecb089ae0e9
Sodiq-049 <sodiqogunmola049@gmail.com>
2023-07-16 23:13:30 +0100

    fix issue

--- index.js
+++ index.js
@@ -20,22 +20,6 @@ function displayRecipes(recipes) {
           .join(", ")}
     `;
 
-        var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById('searchInput');
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("recipe-list");
-        li = ul.getElementsByTagName('li');
-      
-        for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByTagName("a")[0];
-          txtValue = a.textContent || a.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-          } else {
-            li[i].style.display = "none";
-          }
-        }
-
     recipeLinkEl = document.createElement("a");
     recipeLinkEl.href = recipe.sourceUrl;
     recipeLinkEl.innerText = "View Recipe";
