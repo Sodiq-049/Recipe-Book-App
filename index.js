d068c441761e2e42d9bf04b307e54f6e21a4ac36
Sodiq-049 <sodiqogunmola049@gmail.com>
2023-07-16 21:42:31 +0100

    fix issue

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const API_KEY = "275d58779ccf4e22af03e792e8819fff";
 const recipeListEl = document.getElementById("recipe-list");
 
-const searchInput = document.getElementById("searchInput");
-const searchButton = document.getAnimations("searchButton");
-
 function displayRecipes(recipes) {
   recipeListEl.innerHTML = "";
   recipes.forEach((recipe) => {
