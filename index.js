a01a2e57687d1604c2d77d450b6bb2868431ab08
Sodiq-049 <sodiqogunmola049@gmail.com>
2023-07-16 22:46:27 +0100

    fix issue

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ function displayRecipes(recipes) {
           .join(", ")}
     `;
 
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('searchInput');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("recipe-list");
+        li = ul.getElementsByTagName('li');
+      
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+
     recipeLinkEl = document.createElement("a");
     recipeLinkEl.href = recipe.sourceUrl;
     recipeLinkEl.innerText = "View Recipe";
@@ -46,23 +62,5 @@ async function init() {
   const recipes = await getRecipes();
   displayRecipes(recipes);
 }
-
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-  
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
 
 init();
